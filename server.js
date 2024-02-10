818aacf058dfa231af740c309419eb2dd5d22f9b
Tinotenda Nyamande <tnyamande@IMGSOL.co.zw>
2024-02-10 09:21:32 +0200

    workout routes setup

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
+const workoutRoutes = require("./routes/workout")
 
 const express = require("express")
 
 
 const app = express();
 
+
+app.use((req,res,next)=>{
+    console.log("Path",req.path)
+    console.log("Method",req.method)
+    next()
+})
+
 app.listen(process.env.PORT, ()=>{
     console.log("Listening on port 4000")
 })
-
-app.get("/",(req,res)=>{
-    res.json({mssg:"Welcome to my app"})
-})
+app.use(express.json())
+app.use("/api/workouts" ,workoutRoutes)
