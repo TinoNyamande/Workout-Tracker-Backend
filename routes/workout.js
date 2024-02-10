25510bc6fff4d58710f354ad48601ba9b3ac68d6
Tinotenda Nyamande <tnyamande@IMGSOL.co.zw>
2024-02-10 10:00:23 +0200

    Add models and schemas and database connection

--- routes/workout.js
+++ routes/workout.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const Workout = require("./../Models/Workout")
 
 const router = express.Router();
 
@@ -10,8 +11,15 @@ router.get("/:id" , (req,res)=>{
     res.json({msg:"Single workout"})
 })
 
-router.post("/",(req,res)=>{
-    res.json({msg:"Post new workout"})
+router.post("/",async (req,res)=>{
+    const {title , reps ,load} = req.body;
+    try {
+         const workout = await Workout.create({title,reps,load})
+         res.status(200).json(workout)
+    }catch (error) {
+             res.status(400).json({error:error.message})
+    }
+  //  res.json({msg:"Post new workout"})
 })
 
 router.delete("/:id",(req,res)=>{
--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 require("dotenv").config();
-const workoutRoutes = require("./routes/workout")
-
-const express = require("express")
-
+const workoutRoutes = require("./routes/workout");
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Path", req.path);
+  console.log("Method", req.method);
+  next();
+});
 
-app.use((req,res,next)=>{
-    console.log("Path",req.path)
-    console.log("Method",req.method)
-    next()
-})
-
-app.listen(process.env.PORT, ()=>{
-    console.log("Listening on port 4000")
-})
-app.use(express.json())
-app.use("/api/workouts" ,workoutRoutes)
+app.use(express.json());
+app.use("/api/workouts", workoutRoutes);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((response) => {
+    app.listen(process.env.PORT, () => {
+      console.log("Listening on port 4000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
